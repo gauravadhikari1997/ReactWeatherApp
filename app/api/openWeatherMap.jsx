ad2517735b92728133b95397c9bf93205ef62446
Gaurav Adhikari <avjadhikari@gmail.com>
2018-06-29 19:48:19 +0530

    made https in Original url

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=b269381a9075435709d713c68775bee3&units=metric';
+const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=b269381a9075435709d713c68775bee3&units=metric';
 
 module.exports = {
   getTemp: function(location){
